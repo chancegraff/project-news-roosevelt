a42cf00bb5f9536f37528fc6a9d259e798c7e9fd
Chance Graff <cgraffrun@gmail.com>
2020-06-20 00:11:31 -0500

    Fix allow origin header

--- internal/utils/cors.go
+++ internal/utils/cors.go
@@ -2,13 +2,21 @@ package utils
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/handlers"
 )
 
+// AllowedOrigins ...
+var AllowedOrigins = []string{
+	"localhost",
+	"0.0.0.0",
+	"127.0.0.1",
+}
+
 // CORSOrigin ...
 var CORSOrigin = handlers.AllowedOrigins(
-	[]string{"*"},
+	AllowedOrigins,
 )
 
 // CORSHeaders ...
@@ -26,7 +34,7 @@ var SetCORSPolicy = handlers.CORS(CORSMethods, CORSHeaders, CORSOrigin)
 
 // SetCORSHeaders ...
 func SetCORSHeaders(writer http.ResponseWriter) {
-	writer.Header().Set("Access-Control-Allow-Origin", "*")
+	writer.Header().Set("Access-Control-Allow-Origin", strings.Join(AllowedOrigins, ","))
 	writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, HEAD")
 	writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Requested-With, X-Token-Auth, Authorization")
 }
